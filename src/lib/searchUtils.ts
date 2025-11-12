e839bab0591391dbaf3581b47f13e8c54cce9498
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-11-12 19:18:14 +0530

    feat(app): add searxng support

--- src/lib/searchUtils.ts
+++ src/lib/searchUtils.ts
@@ -3,34 +3,75 @@
 import { encode } from "@toon-format/toon";
 
 export const searchImage = async (query: string): Promise<string> => {
-  const response = await fetch("https://google.serper.dev/images", {
-    method: "POST",
-    headers: {
-      "X-API-KEY": process.env.SERPER_API_KEY || "",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ q: query }),
-  });
+  if (process.env.SEARXNG_API_URL) {
+    const res = await fetch(`${process.env.SEARXNG_API_URL}/search?q=${encodeURIComponent(
+      query,
+    )}&engines=${encodeURIComponent('google images,bing images')}&format=json`);
 
-  if (!response.ok) {
-    throw new Error(
-      `Image search failed for "${query}": ` + response.statusText,
-    );
+    if (!res.ok) {
+      throw new Error(
+        `Image search failed for "${query}": ` + res.statusText,
+      );
+    }
+
+    const data = await res.json()
+
+    if (data.results && data.results.length > 0) {
+      return data.results[0].img_src
+    }
+
+    return ""
+  } else if (process.env.SERPER_API_KEY) {
+    const res = await fetch("https://google.serper.dev/images", {
+      method: "POST",
+      headers: {
+        "X-API-KEY": process.env.SERPER_API_KEY || "",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ q: query }),
+    });
+
+    if (!res.ok) {
+      throw new Error(
+        `Image search failed for "${query}": ` + res.statusText,
+      );
+    }
+
+    const data = await res.json();
+
+    if (data.images && data.images.length > 0) {
+      return data.images[0].imageUrl
+    }
+
+    return "";
+  } else {
+    throw new Error("No search engine configured, please configure it via the env file.")
   }
-
-  const data = await response.json();
-
-  if (data.images && data.images.length > 0) {
-    const imageUrl = data.images[0].imageUrl;
-
-    return imageUrl;
-  }
-
-  return "";
 };
 
 export const searchWeb = async (query: string): Promise<string> => {
-  const response = await fetch("https://google.serper.dev/search", {
+  if (process.env.SEARXNG_API_URL) {
+    const res = await fetch(`${process.env.SEARXNG_API_URL}/search?q=${encodeURIComponent(
+      query,
+    )}&format=json`);
+
+    if (!res.ok) {
+      throw new Error(
+        `Web search failed for "${query}": ` + res.statusText,
+      );
+    }
+
+    const data = await res.json()
+
+    if (data.results && data.results.length > 0) {
+      return encode({
+        results: data.results
+      })
+    }
+
+    return ""
+  } else if (process.env.SERPER_API_KEY) {
+    const res = await fetch("https://google.serper.dev/search", {
     method: "POST",
     headers: {
       "X-API-KEY": process.env.SERPER_API_KEY || "",
@@ -39,11 +80,11 @@ export const searchWeb = async (query: string): Promise<string> => {
     body: JSON.stringify({ q: query }),
   });
 
-  if (!response.ok) {
-    throw new Error(`Web search failed for "${query}": ` + response.statusText);
+  if (!res.ok) {
+    throw new Error(`Web search failed for "${query}": ` + res.statusText);
   }
 
-  const data = await response.json();
+  const data = await res.json();
 
   if (data.organic && data.organic.length > 0) {
     return encode({
@@ -52,4 +93,7 @@ export const searchWeb = async (query: string): Promise<string> => {
   }
 
   return "No results found.";
+  } else {
+    throw new Error("No search engine configured, please configure it via the env file.")
+  }
 };
